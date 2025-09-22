9fe80fe47018e90570062570aadb8c07cc141d3a
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2025-09-22 22:17:10 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,7 +5,7 @@ import { navLinks } from "@/constants/navlinks";
 
 export default function Navbar() {
 	return (
-		<header className="container py-[0.5rem] w-full fixed top-0 z-[999]">
+		<header className="container py-[0.5rem] w-full fixed top-0 z-[999] bg-[#080808]">
 			<nav className="px-[1.25rem] md:px-[4.5rem] flex justify-between items-center">
 				<Link href="/#home">
 					<div className="w-[3.75rem] h-[3.75rem] md:w-[5rem] md:h-[5rem] relative">
