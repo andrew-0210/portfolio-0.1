4f873abfde98892f8486f929c01cc5ba4a36afe4
Andrew Monthero <andrewmonthero21@gmail.com>
2025-10-08 19:36:53 +0530

    projects updated

--- src/components/section/Projects.tsx
+++ src/components/section/Projects.tsx
@@ -6,6 +6,7 @@ import gsap from "gsap";
 import { projects } from "@/constants/data";
 import Link from "next/link";
 import { useRef } from "react";
+import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -38,28 +39,50 @@ export default function Projects() {
 				Featured Works
 			</h2>
 			<div className="mt-[1.875rem] md:mt-[3.75rem]">
-				{projects.map((project, i) => (
-					<Link
-						href={project.href}
-						className="h-[60vh] md:h-[85vh] flex items-end p-[2rem] md:pb-[4rem] bg-[#262626] mb-[1.875rem] sm:mb-0 opacity-0 translate-y-30 rounded-[20px] sm:rounded-none justify-center hover:ring-3 hover:ring-[#BC4200]"
-						key={i}
-						ref={(el) => {
-							if (el) projectRef.current[i] = el;
-						}}>
-						<div className="block">
-							<p className="font-display text-center tracking-normal text-[1.5rem] md:text-[2rem] leading-[30px] whitespace-pre-line">
-								<span className="text-[1.5rem]">
-									({i <= 9 ? "0" + (i + 1) : i + 1})
-								</span>{" "}
-								<span className="font-sans tracking-[-0.8] font-medium md:tracking-[-2.5] ">
-									{project.title} |
-								</span>{" "}
-								{project.subTitle}
-							</p>
-						</div>
-						<div className="img"></div>
-					</Link>
-				))}
+				{projects.map((project, i) =>
+					i < 4 ? (
+						<Link
+							href={project.href}
+							className="relative h-[60vh] md:h-[85vh] flex items-end p-[2rem] md:pb-[4rem] bg-[#262626] mb-[1.875rem] sm:mb-0 opacity-0 translate-y-30 rounded-[20px] lg:rounded-none justify-center hover:ring-3 hover:ring-[#BC4200] overflow-hidden"
+							key={i}
+							ref={(el) => {
+								if (el) projectRef.current[i] = el;
+							}}>
+							<div className="block z-[999]">
+								<p className="font-display text-center tracking-normal text-[1.5rem] md:text-[2rem] leading-[30px] whitespace-pre-line">
+									<span className="text-[1.5rem]">
+										({i <= 9 ? "0" + (i + 1) : i + 1})
+									</span>{" "}
+									<span className="font-sans tracking-[-0.8] font-medium md:tracking-[-2.5] ">
+										{project.title} |
+									</span>{" "}
+									{project.subTitle}
+								</p>
+							</div>
+							<div>
+								<Image
+									src={project.imgUrl}
+									alt={project.imgUrl}
+									fill
+									className="object-fill"
+								/>
+							</div>
+						</Link>
+					) : (
+						<Link
+							href={project.href}
+							className="font-display text-center tracking-normal text-[1.5rem] md:text-[2rem] leading-[30px] whitespace-pre-line p-[2rem] inline-block border mb-[1.5rem] rounded-[12px] border-[#999999] hover:ring-3 hover:ring-[#BC4200]"
+							key={i}>
+							<span className="text-[1.5rem]">
+								({i <= 9 ? "0" + (i + 1) : i + 1})
+							</span>{" "}
+							<span className="font-sans tracking-[-0.8] font-medium md:tracking-[-2.5]">
+								{project.title} |
+							</span>{" "}
+							<span>{project.subTitle}</span>
+						</Link>
+					),
+				)}
 			</div>
 		</section>
 	);
--- src/constants/data.ts
+++ src/constants/data.ts
@@ -36,21 +36,43 @@ export const projects = [
 		href: "https://medium.com/@andrewmonthero/creative-cooks-ui-ux-case-study-5fb14d3332a1",
 		title: "Creative Cooks",
 		subTitle: "Curated recipe-box delivery app",
+		imgUrl:
+			"https://ik.imagekit.io/imagestore0210/portfolio/Thumbnail.png?updatedAt=1759930698278",
 	},
 	{
 		href: "https://myimplantdentist.com.au/",
 		title: "My Implant Dentist",
 		subTitle: "Dental Clinic Website based in Australia",
+		imgUrl:
+			"https://ik.imagekit.io/imagestore0210/portfolio/Thumbnail-1.png?updatedAt=1759930704646",
 	},
 	{
 		href: "https://www.figma.com/design/pTMSRLniQBHdSDsarPBJkV/HRMS-software?node-id=89-851&t=tYmxUXFdc0P7RS3a-4",
 		title: "Uni5",
 		subTitle: "Human Resource Management Web App",
+		imgUrl:
+			"https://ik.imagekit.io/imagestore0210/portfolio/Thumbnail-2.png?updatedAt=1759930687020",
 	},
 	{
 		href: "https://www.figma.com/design/dMXi7CRWqFCLZeGw10oCqc/PT-Ecological-CRM?node-id=929-32307&t=aU2QdeLuVNIjrm4h-4",
+		title: "PT Ecological Internal Software",
+		subTitle: "Company Management Software",
+		imgUrl:
+			"https://ik.imagekit.io/imagestore0210/portfolio/Thumbnail.png?updatedAt=1759930698278",
+	},
+	{
+		href: "https://www.figma.com/design/BngSVkRBOw7cv0yRpBXVRr/BuildNest?node-id=148-11363&t=vlDfX1Sl9O1yJooi-4",
 		title: "Postsales Software",
 		subTitle: "ERP software for real estate management",
+		imgUrl:
+			"https://ik.imagekit.io/imagestore0210/portfolio/Thumbnail.png?updatedAt=1759930698278",
+	},
+	{
+		href: "https://www.figma.com/design/VjM5cFbRpuUH876un8s0t7/Aviation-obstruction?node-id=538-1373&t=qZht7AxqdmJgXNhp-4",
+		title: "Aviation Obstruction Light Monitoring System",
+		subTitle: "Obstruction light Monitoring Software",
+		imgUrl:
+			"https://ik.imagekit.io/imagestore0210/portfolio/Thumbnail.png?updatedAt=1759930698278",
 	},
 ];
 
