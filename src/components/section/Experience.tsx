f088f39803a3cf8cbd3debfd46e8b374c343b8f8
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2026-02-20 12:59:20 +0530

    Refactor Experience component to use state for duration

--- src/components/section/Experience.tsx
+++ src/components/section/Experience.tsx
@@ -1,51 +1,65 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { companyList } from "@/constants/data";
 
 export default function Experience() {
- const startDate = new Date(2021, 10);
-  const today = new Date();
+  const [experience, setExperience] = useState({
+    years: 0,
+    months: 0,
+  });
 
-  let years = today.getFullYear() - startDate.getFullYear();
-  let months = today.getMonth() - startDate.getMonth();
+  useEffect(() => {
+    const startDate = new Date(2021, 10); // November 2021
+    const today = new Date();
 
-  if (months < 0) {
-    years--;
-    months += 12;
-  }
-	
-	return (
-		<section className="container py-[4.385rem] px-[1.125rem]">
-			<h2 className="font-display text-center text-[2rem] md:text-[3rem] text-[#999999]">
-				Experience{" "}
-				<span className="text-[1.5rem] md:text-[2.25rem] ">
-					({years}.{months}{" "}years)
-				</span>
-			</h2>
-			<div className="md:grid md:grid-cols-12">
-				<div className="flex mt-[1.875rem] md:mt-[3.75rem] gap-[2.5rem] flex-col-reverse md:col-span-8 md:col-start-3 ">
-					{companyList.map((skill, i) => (
-						<div
-							className=" flex justify-between items-center gap-[1rem]"
-							key={i}>
-							<div className="flex items-center gap-[0.5rem] flex-1/4">
-								<span className="font-display text-[1.25rem] md:text-[1.5rem]">
-									({i <= 9 ? "0" + i : i})
-								</span>
-								<div className="">
-									<p className="text-[1rem] md:text-[1.5rem] leading-[21.6px] font-medium tracking-[-0.8]">
-										{skill.company}
-									</p>
-									<p className="text-[0.938rem] md:text-[1.125rem] text-[#999999] tracking-[-0.8] font-medium">
-										{skill.designation}
-									</p>
-								</div>
-							</div>
-							<div className="text-[0.938rem] md:text-[1.125rem] text-[#999999] tracking-[-0.8] font-medium flex-1 text-end">
-								{skill.duration}
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</section>
-	);
+    let years = today.getFullYear() - startDate.getFullYear();
+    let months = today.getMonth() - startDate.getMonth();
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    setExperience({ years, months });
+  }, []);
+
+  return (
+    <section className="container py-[4.385rem] px-[1.125rem]">
+      <h2 className="font-display text-center text-[2rem] md:text-[3rem] text-[#999999]">
+        Experience{" "}
+        <span className="text-[1.5rem] md:text-[2.25rem]">
+          ({experience.years} years {experience.months} months)
+        </span>
+      </h2>
+
+      <div className="md:grid md:grid-cols-12">
+        <div className="flex mt-[1.875rem] md:mt-[3.75rem] gap-[2.5rem] flex-col-reverse md:col-span-8 md:col-start-3 ">
+          {companyList.map((skill, i) => (
+            <div
+              className="flex justify-between items-center gap-[1rem]"
+              key={i}
+            >
+              <div className="flex items-center gap-[0.5rem] flex-1/4">
+                <span className="font-display text-[1.25rem] md:text-[1.5rem]">
+                  ({i <= 9 ? "0" + i : i})
+                </span>
+                <div>
+                  <p className="text-[1rem] md:text-[1.5rem] leading-[21.6px] font-medium tracking-[-0.8]">
+                    {skill.company}
+                  </p>
+                  <p className="text-[0.938rem] md:text-[1.125rem] text-[#999999] tracking-[-0.8] font-medium">
+                    {skill.designation}
+                  </p>
+                </div>
+              </div>
+              <div className="text-[0.938rem] md:text-[1.125rem] text-[#999999] tracking-[-0.8] font-medium flex-1 text-end">
+                {skill.duration}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
